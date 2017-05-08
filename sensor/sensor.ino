115205ce7d8bf0f5cd2427555880d9057b759218
Jordi Petit <jordi.petit@gmail.com>
2017-05-08 23:43:12 +0200

    Fix bugs
    This version seems to work well!

--- sensor/sensor.ino
+++ sensor/sensor.ino
@@ -14,8 +14,9 @@
 #include <SoftwareSerial.h>
 
 
-// Polling period
-const int period = 10 * 1000;
+// Polling period 
+const unsigned long period = 300000L;   // (5 minutes)
+//const unsigned long period = 10000L;  // (10 seconds)
 
 // Name of the wireless network
 const char* ssid = SSID;
@@ -54,6 +55,7 @@ const int MaxLen = 128;
 // The DHT11 sensor device
 SimpleDHT11 dht11;
 
+
 // The ESP01 wifi device
 SoftwareSerial wifi(Pin_RX, Pin_TX);
 
@@ -84,21 +86,23 @@ int read_light() {
 
 void wifi_readline(char* data) {
     int p = 0;
-    while (true) {
-        while (wifi.available()) {
+    bool c13 = false;
+    bool c10 = false;
+    while (not c13) {
+        if (wifi.available()) {
             char c = wifi.read();
-            if (c == 10) return;
-            if (c == 13) continue;
-            data[p++] = c;
+            if (c == 10) c10 = true;
+            else if (c == 13) c13 = true;
+            else data[p++] = c;
         }
     }
+    data[p] = 0;
 }
 
 
 bool wifi_command(const char* command) {
-    wifi.print(command);
+    wifi.println(command);
     char line[MaxLen];
-    memset(line, 0, MaxLen);
     while (true) {
         wifi_readline(line);
         Serial.println(line);
@@ -112,17 +116,28 @@ bool wifi_command(const char* command) {
 
 void send_wifi(const char* data) {
     char cmd[MaxLen];
-    snprintf(cmd, MaxLen, "AT+CIPSTART=\"TCP\",\"%s\",%d\r\n", server_ip, port);
+    snprintf(cmd, MaxLen, "AT+CIPSTART=\"TCP\",\"%s\",%d", server_ip, port);
     wifi_command(cmd);
     int length = strlen(data);
     char str[MaxLen];
-    sprintf(str, "AT+CIPSEND=%d\r\n", length);
-    wifi_command(str); delay(100);
-    wifi.print(data);
-    wifi.print("\r\n"); Serial.println(data); delay(100);
-    wifi_command("AT+CIPCLOSE\r\n");delay(100);
+    sprintf(str, "AT+CIPSEND=%d", length);
+    wifi_command(str);
+    wifi.println(data);
+    Serial.println(data);
 }
 
+
+// Make the Pin_Led flash a bit to signal phases.
+
+void led_flash() {
+    Serial.println("# leds flashing");
+    for (int i = 0; i < 20; ++i) {
+        digitalWrite(Pin_Led, HIGH);
+        delay(250);
+        digitalWrite(Pin_Led, LOW);
+        delay(250);
+    }
+}
 
 
 // Arduino setup code
@@ -130,11 +145,14 @@ void send_wifi(const char* data) {
 void setup() {
     // Setup Serial (console)
     Serial.begin(9600);
-    Serial.println("# ClimaLlar sensor starting");
+    Serial.println("# clima-llar sensor starting");
 
     // Setup LED
     pinMode(Pin_Led, OUTPUT);
     digitalWrite(Pin_Led, LOW);
+
+    // This LED dance signals start of setup.
+    led_flash();
 
     // Setup Photoresistor
     pinMode(Pin_PhRes, INPUT);
@@ -144,45 +162,40 @@ void setup() {
     wifi.begin(9600);
 
     Serial.println("# Wifi test");
-    wifi_command("AT\r\n");
+    wifi_command("AT");
 
+/*  The following command gives me trouble.
     Serial.println("# Wifi info:");
-    wifi_command("AT+GMR\r\n");
+    wifi_command("AT+GMR");
+*/
 
     Serial.println("# Wifi mode:");
-    wifi_command("AT+CWMODE?\r\n");
+    wifi_command("AT+CWMODE?");
 
-    if (true) {
-        Serial.println("# Wifi list of access points:");
-        wifi_command("AT+CWLAP\r\n");
+    Serial.println("# Wifi list of access points:");
+    wifi_command("AT+CWLAP");
 
-        Serial.println("# Wifi join access point:");
-        char str[100];
-        sprintf(str, "AT+CWJAP=\"%s\",\"%s\"\r\n", ssid, pswd);
-        wifi_command(str);
-    }
+    Serial.println("# Wifi join access point:");
+    char str[MaxLen];
+    sprintf(str, "AT+CWJAP=\"%s\",\"%s\"", ssid, pswd);
+    wifi_command(str);
 
     Serial.println("# Wifi info:");
-    wifi_command("AT+CWJAP?\r\n");
+    wifi_command("AT+CWJAP?");
 
     Serial.println("# Wifi info:");
-    wifi_command("AT+CIPSTATUS\r\n");
+    wifi_command("AT+CIPSTATUS");
 
     Serial.println("# Wifi info:");
-    wifi_command("AT+CIFSR\r\n");
+    wifi_command("AT+CIFSR");
 
     Serial.println("# Wifi info:");
-    wifi_command("AT+CIPSTA\r\n");
+    wifi_command("AT+CIPSTA?");
 
     Serial.println("# Wifi done");
 
-    // This LED blinking signals end of setup.
-    for (int i = 0; i < 20; ++i) {
-        digitalWrite(Pin_Led, HIGH);
-        delay(250);
-        digitalWrite(Pin_Led, LOW);
-        delay(250);
-    }
+    // This LED dance signals end of setup.
+    led_flash();
 }
 
 
@@ -196,7 +209,7 @@ void loop() {
     read_DHT11(temperature, humidity);
     int light = read_light();
     char data[MaxLen];
-    snprintf(data, MaxLen, "%d:%d:%d:%d", key, temperature, humidity, light);
+    snprintf(data, MaxLen, "%s:%d:%d:%d", key, temperature, humidity, light);
     send_wifi(data);
     digitalWrite(Pin_Led, LOW);
     delay(period);
