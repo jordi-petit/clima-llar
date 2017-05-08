28475d33ccb3bae97f34ae0447c040743ed5a6db
Jordi Petit <jordi.petit@gmail.com>
2017-05-08 10:05:49 +0200

    Minor

--- server/client.py
+++ server/client.py
@@ -10,7 +10,7 @@ import socket
 
 
 def send(ip, port, data):
-    print('send "%s" to %s:%s' % (data, ip, port))
+    print("send '%s' to %s:%s" % (data, ip, port))
     sock = socket.socket()
     sock.connect((ip, port))
     sock.send(data.encode())
@@ -23,7 +23,7 @@ def main():
 
     parser = argparse.ArgumentParser(description="Sends a request to the ClimaLlar server.")
 
-    parser.add_argument('data', type=str, nargs=1, help='data to send to the server')
+    parser.add_argument("data", type=str, nargs=1, help="data to send to the server")
     parser.add_argument("-s", "--server", type=str, help="server name or IP", default=server)
     parser.add_argument("-p", "--port", type=int, help="port number", default=9876)
 
