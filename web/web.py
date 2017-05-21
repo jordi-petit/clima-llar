8f609ea414ee7b4267b803e502c9d094fb5b8f25
Jordi Petit <jordi.petit@gmail.com>
2017-05-21 21:54:34 +0200

    Improvements

--- web/web.py
+++ web/web.py
@@ -12,10 +12,12 @@ app.config["DATABASE"] = os.path.expanduser("~/clima-llar.db")
 
 
 def current_datetime():
+    """Returns the current date and time in my standard format."""
     return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 
 def current_date():
+    """Returns the current date in my standard format."""
     return datetime.datetime.now().strftime("%Y-%m-%d")
 
 
@@ -34,13 +36,14 @@ def close_db(error):
 
 
 def get_db():
-    """Opens a new database connection if there is none yet for the current application context."""
+    """Opens a new database connection if necessary."""
     if not hasattr(g, "sqlite_db"):
         g.sqlite_db = connect_db()
     return g.sqlite_db
 
 
-def fetchone(*sql):
+def get_row(*sql):
+    """Gets one row of a sql query, returning a dict, or None."""
     dbc = get_db()
     cur = dbc.execute(*sql)
     row = cur.fetchone()
@@ -50,14 +53,27 @@ def fetchone(*sql):
         return dict(row)
 
 
-def fetchall(*sql):
+def get_one(*sql):
+    """Gets one element of a sql query, returning a value."""
+    dbc = get_db()
+    cur = dbc.execute(*sql)
+    row = cur.fetchone()
+    if row is None:
+        return None
+    else:
+        return row[0]
+
+
+def get_all(*sql):
+    """Gets all rows of a sql query, returning a list of dicts."""
     dbc = get_db()
     cur = dbc.execute(*sql)
     rows = cur.fetchall()
     return [dict(row) for row in rows]
 
 
-def fetchcol(*sql):
+def get_col(*sql):
+    """Gets a column of a sql query, returning a list of values."""
     dbc = get_db()
     cur = dbc.execute(*sql)
     rows = cur.fetchall()
@@ -65,7 +81,29 @@ def fetchcol(*sql):
 
 
 def day_readings(place_id, date):
-    return jsonify(fetchall("select moment, temperature, humidity, light from readings where date(moment) = ? and place_id = ? order by moment", [date, place_id]))
+    """Returns th readings for a given place and date."""
+    return jsonify(get_all(
+        """
+        SELECT moment, temperature, humidity, light
+        FROM Readings
+        WHERE DATE(moment) = ?
+        AND place_id = ?
+        ORDER BY moment
+        """,
+        [date, place_id]
+    ))
+
+
+def place_exists(place_id):
+    c = get_one(
+        """
+        SELECT COUNT(*)
+        FROM Places
+        WHERE place_id = ?
+        """,
+        [place_id]
+    )
+    return c != 0
 
 
 @app.route("/")
@@ -76,7 +114,7 @@ def root():
 
 @app.route("/app.js")
 def app_js():
-    """Returns the app script."""
+    """Returns the app script of the web page."""
     return send_file("app.js")
 
 
@@ -89,12 +127,23 @@ def bower_components(path):
 @app.route("/api/<place_id>")
 def get_current(place_id):
     """Returns the latest readings of a given place."""
-    return jsonify(fetchone("select moment, temperature, humidity, light from readings where place_id = ? order by moment desc limit 1", [place_id]))
+    assert place_exists(place_id)
+    return jsonify(get_row(
+        """
+        SELECT moment, temperature, humidity, light
+        FROM Readings
+        WHERE place_id = ?
+        ORDER BY moment DESC
+        LIMIT 1
+        """,
+        [place_id]
+    ))
 
 
 @app.route("/api/<place_id>/submit")
 def submit(place_id):
     """Adds a new reading for a given place_id."""
+    assert place_exists(place_id)
     try:
         moment = current_datetime()
         ip = request.remote_addr
@@ -102,8 +151,14 @@ def submit(place_id):
         humidity = float(request.args["humidity"])
         light = float(request.args["light"])
         db = get_db()
-        db.execute('insert into readings (moment, ip, place_id, temperature, humidity, light) values (?,?,?,?,?,?)',
-            [moment, ip, place_id, temperature, humidity, light])
+        db.execute(
+            """
+            INSERT INTO Readings (moment, ip, place_id,
+                temperature, humidity, light)
+            VALUES (?,?,?,?,?,?)
+            """,
+            [moment, ip, place_id, temperature, humidity, light]
+        )
         db.commit()
         return "ok"
     except Exception as exc:
@@ -114,12 +169,22 @@ def submit(place_id):
 @app.route("/api/<place_id>/dates")
 def get_dates(place_id):
     """Returns the available dates with readings of a given place."""
-    return jsonify(fetchcol("select distinct date(moment) as date from readings where place_id = ? order by date desc", [place_id]))
+    assert place_exists(place_id)
+    return jsonify(get_col(
+        """
+        SELECT DISTINCT DATE(moment) AS date
+        FROM readings
+        WHERE place_id = ?
+        ORDER BY date DESC
+        """,
+        [place_id]
+    ))
 
 
 @app.route("/api/<place_id>/dates/today")
 def get_today(place_id):
     """Returns the readings of a given place for today."""
+    assert place_exists(place_id)
     today = current_date()
     return day_readings(place_id, today)
 
@@ -127,6 +192,7 @@ def get_today(place_id):
 @app.route("/api/<place_id>/dates/<date>")
 def get_date(place_id, date):
     """Returns the readings of a given place and a given date."""
+    assert place_exists(place_id)
     return day_readings(place_id, date)
 
 
