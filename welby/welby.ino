96177e952469396fa781edd846d7061cca61ef18
Jordi Petit <jordi.petit@gmail.com>
2017-06-19 22:10:28 +0200

    Improve

--- welby/welby.ino
+++ welby/welby.ino
@@ -1,7 +1,7 @@
+#include <ESP8266WiFi.h>
 #include <SimpleDHT.h>
 #include <IRrecv.h>
 //#include "queue.hh"
-
 
 // Pin for the leds
 const int Pin_LedR = D6;
@@ -33,58 +33,106 @@ int temperature;
 int humidity;
 int light;
 
-
-
 void translateIR() // takes action based on IR code received
 
 // describing Remote IR codes
 
 {
 
-  switch(results.value)
+    switch (results.value)
 
-  {
-  case 0xFFA25D: Serial.println("POWER"); break;
-  case 0xFFE21D: Serial.println("FUNC/STOP"); break;
-  case 0xFF629D: Serial.println("VOL+"); break;
-  case 0xFF22DD: Serial.println("FAST BACK");    break;
-  case 0xFF02FD: Serial.println("PAUSE");    break;
-  case 0xFFC23D: Serial.println("FAST FORWARD");   break;
-  case 0xFFE01F: Serial.println("DOWN");    break;
-  case 0xFFA857: Serial.println("VOL-");    break;
-  case 0xFF906F: Serial.println("UP");    break;
-  case 0xFF9867: Serial.println("EQ");    break;
-  case 0xFFB04F: Serial.println("ST/REPT");    break;
-  case 0xFF6897: Serial.println("0");    break;
-  case 0xFF30CF: Serial.println("1");    analogWrite(Pin_LedR, 255); break;
-  case 0xFF18E7: Serial.println("2");    analogWrite(Pin_LedR, 128);break;
-  case 0xFF7A85: Serial.println("3");    analogWrite(Pin_LedR, 0);break;
-  case 0xFF10EF: Serial.println("4");    analogWrite(Pin_LedG, 255); break;
-  case 0xFF38C7: Serial.println("5");    analogWrite(Pin_LedG, 128); break;
-  case 0xFF5AA5: Serial.println("6");    analogWrite(Pin_LedG, 0); break;
-  case 0xFF42BD: Serial.println("7");    analogWrite(Pin_LedB, 255); break;
-  case 0xFF4AB5: Serial.println("8");    analogWrite(Pin_LedB, 128); break;
-  case 0xFF52AD: Serial.println("9");    analogWrite(Pin_LedB, 0); break;
-  case 0xFFFFFFFF: Serial.println(" REPEAT");break;
+    {
+    case 0xFFA25D:
+        Serial.println("POWER");
+        break;
+    case 0xFFE21D:
+        Serial.println("FUNC/STOP");
+        break;
+    case 0xFF629D:
+        Serial.println("VOL+");
+        break;
+    case 0xFF22DD:
+        Serial.println("FAST BACK");
+        break;
+    case 0xFF02FD:
+        Serial.println("PAUSE");
+        break;
+    case 0xFFC23D:
+        Serial.println("FAST FORWARD");
+        break;
+    case 0xFFE01F:
+        Serial.println("DOWN");
+        break;
+    case 0xFFA857:
+        Serial.println("VOL-");
+        break;
+    case 0xFF906F:
+        Serial.println("UP");
+        break;
+    case 0xFF9867:
+        Serial.println("EQ");
+        break;
+    case 0xFFB04F:
+        Serial.println("ST/REPT");
+        break;
+    case 0xFF6897:
+        Serial.println("0");
+        break;
+    case 0xFF30CF:
+        Serial.println("1");
+        analogWrite(Pin_LedR, 255);
+        break;
+    case 0xFF18E7:
+        Serial.println("2");
+        analogWrite(Pin_LedR, 128);
+        break;
+    case 0xFF7A85:
+        Serial.println("3");
+        analogWrite(Pin_LedR, 0);
+        break;
+    case 0xFF10EF:
+        Serial.println("4");
+        analogWrite(Pin_LedG, 255);
+        break;
+    case 0xFF38C7:
+        Serial.println("5");
+        analogWrite(Pin_LedG, 128);
+        break;
+    case 0xFF5AA5:
+        Serial.println("6");
+        analogWrite(Pin_LedG, 0);
+        break;
+    case 0xFF42BD:
+        Serial.println("7");
+        analogWrite(Pin_LedB, 255);
+        break;
+    case 0xFF4AB5:
+        Serial.println("8");
+        analogWrite(Pin_LedB, 128);
+        break;
+    case 0xFF52AD:
+        Serial.println("9");
+        analogWrite(Pin_LedB, 0);
+        break;
+    case 0xFFFFFFFF:
+        Serial.println(" REPEAT");
+        break;
 
-  default:
-    Serial.println(" other button   ");
+    default:
+        Serial.println(" other button   ");
 
-  }// End Case
+    } // End Case
 
-  delay(500); // Do not get immediate repeat
-
+    delay(500); // Do not get immediate repeat
 
 }
-
-
 
 // Read temperature and humidity from DHT11 and light from photoresistor
 
 void get_readinds() {
     byte temp = 0;
     byte humi = 0;
-    if (dht11.read(Pin_DHT11, &temp, &humi, NULL)) {
+    if (dht11.read(Pin_DHT11, & temp, & humi, NULL)) {
         Serial.println("Read DHT11 failed");
     }
     temperature = temp;
@@ -92,6 +140,25 @@ void get_readinds() {
     light = analogRead(Pin_PhRes);
 }
 
+
+
+void printWifiStatus()
+{
+    // print the SSID of the network you're attached to
+    Serial.print("SSID: ");
+    Serial.println(WiFi.SSID());
+
+    // print IP address
+    IPAddress ip = WiFi.localIP();
+    Serial.print("IP Address: ");
+    Serial.println(ip);
+
+    // print the received signal strength
+    long rssi = WiFi.RSSI();
+    Serial.print("Signal strength (RSSI):");
+    Serial.print(rssi);
+    Serial.println(" dBm");
+}
 
 
 void setup() {
@@ -106,18 +173,66 @@ void setup() {
     pinMode(Pin_LedR, OUTPUT);
     pinMode(Pin_LedG, OUTPUT);
     pinMode(Pin_LedB, OUTPUT);
+    analogWrite(Pin_LedR, 0);
+    analogWrite(Pin_LedG, 0);
+    analogWrite(Pin_LedB, 0);
 
     // Setup IR
     irrecv.enableIRIn();
-}
 
+    // Setup Wifi
+    Serial.print("Connecting to wifi");
+    WiFi.begin("VIRUS64", "filiprim64");
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("Connected");
+    Serial.println("");
+
+    printWifiStatus();
+}
 
 void loop() {
-    if (irrecv.decode(&results))
-      {
+    if (irrecv.decode( & results)) {
         translateIR();
         irrecv.resume();
-      }
+    }
 
+    WiFiClient client;
+
+    const char * server = "clima-llar.potipoti.org";
+
+    if (client.connect(server, 80)) {
+
+        Serial.println("WiFi Client connected ");
+
+        client.print("GET /hello HTTP/1.1\r\n");
+        client.print("Host: clima-llar.potipoti.org\r\n");
+        client.print("\r\n");
+
+        Serial.println("request sent");
+        while (client.connected()) {
+            String line = client.readStringUntil('\n');
+            if (line == "\r") {
+                Serial.println("headers received");
+                break;
+            }
+        }
+        String line = client.readStringUntil('\n');
+
+        Serial.println("reply was:");
+        Serial.println("==========");
+        Serial.println(line);
+        Serial.println("==========");
+        Serial.println("closing connection");
+
+    } else {
+        Serial.println("connext no va");
+    }
+    client.stop();
+
+    delay(30000);
 }
-
